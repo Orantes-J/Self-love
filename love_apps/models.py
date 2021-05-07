22042d725624be7cd31de812c6c89fd42859166b
Juan Orantes <carlos.oran15@hotmail.com>
2021-05-06 19:54:36 -0700

    adjusted image upload path for each models

--- love_apps/models.py
+++ love_apps/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    availabilty = models.CharField(max_length=200, blank=True)
+    quantity = models.IntegerField(default=1, blank=True)
     price  = models.DecimalField(max_digits=5, decimal_places=2)
-    product_image = models.ImageField(upload_to = "static/images/")
+    product_image = models.ImageField(upload_to = "images/product")
 
 class TopPicks(models.Model):
     class Meta:
@@ -13,8 +15,10 @@ class TopPicks(models.Model):
         verbose_name_plural = "Top Picks"
     name = models.CharField(max_length=200)
     description = models.TextField()
+    availabilty = models.CharField(max_length=200, blank=True)
+    quantity = models.IntegerField(default=1, blank=True)
     price  = models.DecimalField(max_digits=5, decimal_places=2)
-    product_image = models.ImageField(upload_to = "static/images/")
+    product_image = models.ImageField(upload_to = "images/toppicks")
 
 class BestSeller(models.Model):
     class Meta:
@@ -22,8 +26,10 @@ class BestSeller(models.Model):
         verbose_name_plural = "Best Sellers"
     name = models.CharField(max_length=200)
     description = models.TextField()
+    availabilty = models.CharField(max_length=200, blank=True)
+    quantity = models.IntegerField(default=1, blank=True)
     price  = models.DecimalField(max_digits=5, decimal_places=2)
-    product_image = models.ImageField(upload_to = "static/images/")
+    product_image = models.ImageField(upload_to = "images/bestseller")
 
 class NewArrivals(models.Model):
     class Meta:
@@ -31,11 +37,15 @@ class NewArrivals(models.Model):
         verbose_name_plural = "New Arrivals"
     name = models.CharField(max_length=200)
     description = models.TextField()
+    availabilty = models.CharField(max_length=200, blank=True)
+    quantity = models.IntegerField(default=1, blank=True)
     price  = models.DecimalField(max_digits=5, decimal_places=2)
-    product_image = models.ImageField(upload_to = "static/images/")
+    product_image = models.ImageField(upload_to = "images/newarrivals")
 
 class ComingSoon(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    availabilty = models.CharField(max_length=200, blank=True)
+    quantity = models.IntegerField(default=1, blank=True)
     price  = models.DecimalField(max_digits=5, decimal_places=2)
-    product_image = models.ImageField(upload_to = "static/images/")
+    product_image = models.ImageField(upload_to = "images/coming")
